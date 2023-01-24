feb0375d0b414e8ef90f81475932b51fed0a9f1c
natalia-grigorovich <lutsenko.ns@gmail.com>
2023-01-24 21:54:05 +0300

    Solution for Car: add method set_engine()

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,13 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02.engine import Engine
+
+
+class Car(Vehicle):
+    engine = None
+
+    def set_engine(volume, pistons):
+        engine = Engine(volume=volume, pistons=pistons)
+        return engine
